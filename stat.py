d87328f71566d5cf13b73f0d7ba2884d0b95e932
OLDecker <ole.decker.od@gmail.com>
2026-03-01 12:00:57 +0100

    cleanup

--- stat.py
+++ stat.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-# This program demonstrates a way to get arguments and options from command line
 import sys
 import pandas as pd
 
-# You should use more appropriately named variable names than me.
-# I just don't know what your options are.
-# In my example two options are just present or not, but the third requires a number to follow.
-# By initalising the option to None, you can later tell that you did not get this option.
-# If you initialise it to a value instead, then that becomes a default value, ready to use. 
 optionA, optionM, optionNumber, optionN, optionB, optionT, optionF = None, None, None, None, None, None, None
 filename = None
 
-# By creating a usage function, you can always call that. Makes it easy to be user friendly
+# functions
 def usage(msg=None):
     # Print a message if there was something specific you want to the user. 
     if msg is not None:
@@ -20,37 +14,6 @@ def usage(msg=None):
     # Exit the program. We can not progress. Makes logic easier elsewhere.
     sys.exit(1)
 
-# Command line parsing
-# This can be done in different ways. This is just simple. Could be a function.
-while len(sys.argv) > 1:
-    arg = sys.argv.pop(1)
-    if arg == '-a':
-        optionA = True
-    elif arg == '-m':
-        optionM = True
-    elif arg == '-n':
-        optionN = True
-    elif arg == '-b':
-        optionB = True
-    elif arg == '-t':
-        optionT = True
-    elif arg == '-f':
-        optionF = True
-    elif arg == '-c':
-        try:
-            # There are possibility for failure here - no argument, not integer
-            optionNumber = int(sys.argv.pop(1))
-            if optionNumber < 1:
-                raise ValueError
-        except:
-            usage()
-    elif filename is None:
-        filename = arg
-    else:
-        usage()
-        
-        
-# functions
 def average(lst):
     if not lst:
         return 0
@@ -93,6 +56,36 @@ def frequent(lst):
     for i in lst:
         counts[i] = counts.get(i, 0) + 1 
     return max(counts, key=counts.get)
+
+# Command line parsing
+while len(sys.argv) > 1:
+    arg = sys.argv.pop(1)
+    if arg == '-a':
+        optionA = True
+    elif arg == '-m':
+        optionM = True
+    elif arg == '-n':
+        optionN = True
+    elif arg == '-b':
+        optionB = True
+    elif arg == '-t':
+        optionT = True
+    elif arg == '-f':
+        optionF = True
+    elif arg == '-c':
+        try:
+            # There are possibility for failure here - no argument, not integer
+            optionNumber = int(sys.argv.pop(1))
+            if optionNumber < 1:
+                raise ValueError
+        except:
+            usage()
+    elif filename is None:
+        filename = arg
+    else:
+        usage()
+        
+        
 
     
 # Working with the options
